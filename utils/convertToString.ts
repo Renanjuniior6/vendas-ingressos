4bee5e52073e4ccdac01faf9f43e5747d2f33c2e
Renan <renanjrr679@gmail.com>
2026-04-13 20:17:04 -0300

    feat: adicionado funcao para converter para string

---
+++ utils/convertToString.ts
@@ -0,0 +1,11 @@
+export function convertToString(value: any): string {
+    if (typeof value === 'string') {
+        return value;
+    }
+    if (typeof value === 'number' || typeof value === 'boolean') {
+        return value.toString();
+    }
+    if (value === null || value === undefined) {
+        return '';
+    }
+};
